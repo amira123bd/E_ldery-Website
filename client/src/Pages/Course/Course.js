f5705b1b560608d9a2f14c0ae1c0923bee4dcb64
amira123bd <bedouiamira123@gmail.com>
2023-01-21 00:02:47 +0100

    Apps.css

--- client/src/Pages/Course/Course.js
+++ client/src/Pages/Course/Course.js
@@ -4,6 +4,9 @@ import "./Course.css";
 const Course = () => {
     return(
       
-      <div>this is the page after picking a lesson</div>)}
+      <div>this is the page after picking a lesson</div>)
+    
+    
+    }
 
 export default Course;
